b0af1019d6ca94b915e9b9e09d886f1cb7b06848
Celal Can Akbulut <67287292+celalcanakblt@users.noreply.github.com>
2024-02-12 02:01:26 +0300

    Create UcgenAlanHesaplama.py

---
+++ Odevler/UcgenAlanHesaplama.py
@@ -0,0 +1,4 @@
+##################### UCGEN ALAN HESAPLAMA ###############################
+taban=int(input("Üçgenin tabanını giriniz : "))
+yukseklik=int(input("Üçgenin yüksekliğini giriniz : "))
+print("Üçgenin alanı = ", (taban*yukseklik) / 2)
