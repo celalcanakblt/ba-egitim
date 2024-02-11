5ed5c196fd1ba4ddcc72e19932cb9d60946bbe6f
Celal Can Akbulut <67287292+celalcanakblt@users.noreply.github.com>
2024-02-12 02:01:58 +0300

    Create celciustofahrenheit.py

---
+++ Odevler/celciustofahrenheit.py
@@ -0,0 +1,4 @@
+##################### CELCIUS TO FAHRENHEIT ###############################
+sicaklik = float(input("Lütfen sıcaklık değeri giriniz :"))
+fah = (1.8 * sicaklik ) + 32
+print("Fahrenheit değerinde sıcaklık : ",fah,"ºF")
