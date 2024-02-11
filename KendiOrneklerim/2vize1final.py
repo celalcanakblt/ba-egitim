17eed0ad28132966c52fa03988c6b07df6876542
Celal Can Akbulut <67287292+celalcanakblt@users.noreply.github.com>
2024-02-12 02:04:41 +0300

    2vize1final.py

---
+++ KendiOrneklerim/2vize1final.py
@@ -0,0 +1,8 @@
+vize1=float(input("Birinci vize gir : "))
+vize2=float(input("Birinci vize gir : "))
+final=float(input("Birinci vize gir : "))
+
+ortalama = (float (vize1)*0.3 + float(vize2)*0.3 + float(final)*0.4)
+ortalama1= (vize1*0.3 + vize2*0.3) + final*0.4
+print("Ortalamanız : {}".format(ortalama))
+print("Ortalamanız : {}".format(ortalama1))
